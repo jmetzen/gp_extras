bdc11e20f6cdc7d3c14d9251aee4c010d041c0c1
Jan Hendrik Metzen <jhm@informatik.uni-bremen.de>
2015-11-27 15:30:19 +0100

    EX Fixing example illustrating manifold kernel on discontinuous function

--- examples/plot_gpr_discontinuity.py
+++ examples/plot_gpr_discontinuity.py
@@ -3,22 +3,22 @@ import numpy as np
 import pylab
 
 from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import RBF
-from sklearn.gaussian_process.kernels_non_stationary import ManifoldKernel
+from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C
+from sklearn_kernels import ManifoldKernel
 
-np.random.seed(0)
+np.random.seed(1)
 
 # Specify Gaussian Process
-kernel = (1e-10, 1.0, 100) \
-    * ManifoldKernel(base_kernel=RBF(0.1), architecture=((1, 2),),
-                     transfer_fct="tanh", max_nn_weight=1)
-gp = GaussianProcessRegressor(kernel=kernel)
-
+kernel = C(1.0, (0.01, 100)) \
+    * ManifoldKernel.construct(base_kernel=RBF(0.1), architecture=((1, 2),),
+                               transfer_fct="tanh", max_nn_weight=1)
+gp = GaussianProcessRegressor(kernel=kernel, alpha=1e-5,
+                              n_restarts_optimizer=10)
 
 X_ = np.linspace(-7.5, 7.5, 100)
 
 # Visualization of prior
-X_nn = kernel.k2._project_manifold(X_[:, None])
+X_nn = gp.kernel.k2._project_manifold(X_[:, None])
 pylab.figure(0, figsize=(8, 8))
 pylab.subplot(2, 1, 1)
 for i in range(X_nn.shape[1]):
@@ -31,7 +31,7 @@ pylab.plot(X_, y_mean, 'k', lw=3, zorder=9)
 pylab.fill_between(X_, y_mean - np.sqrt(np.diag(y_cov)),
                    y_mean + np.sqrt(np.diag(y_cov)),
                    alpha=0.5, color='k')
-y_samples = gp.sample(X_[:, None], 10)
+y_samples = gp.sample_y(X_[:, None], 10)
 pylab.plot(X_, y_samples, color='b', lw=1)
 pylab.xlim(-7.5, 7.5)
 pylab.ylim(-3, 3)
@@ -39,13 +39,12 @@ pylab.title("Prior samples")
 
 
 # Generate data and fit GP
-X = np.random.uniform(-5, 5, 30)[:, None]
+X = np.random.uniform(-5, 5, 40)[:, None]
 y = np.sin(X[:, 0]) + (X[:, 0] > 0)
-old_params = kernel.params
 gp.fit(X, y)
 
 # Visualization of posterior
-X_nn = kernel.k2._project_manifold(X_[:, None])
+X_nn = gp.kernel_.k2._project_manifold(X_[:, None])
 
 pylab.figure(1, figsize=(8, 8))
 pylab.subplot(2, 1, 1)
@@ -59,7 +58,7 @@ pylab.plot(X_, y_mean, 'k', lw=3, zorder=9)
 pylab.fill_between(X_, y_mean - np.sqrt(np.diag(y_cov)),
                    y_mean + np.sqrt(np.diag(y_cov)),
                    alpha=0.5, color='k')
-y_samples = gp.sample(X_[:, None], 10)
+y_samples = gp.sample_y(X_[:, None], 10)
 pylab.plot(X_, y_samples, color='b', lw=1)
 pylab.scatter(X[:, 0], y, c='r', s=50, zorder=10)
 pylab.xlim(-7.5, 7.5)
